04a23880182da5049b65b6b3af446fa947c6fa83
Samuel Thomson <samthomson@btinternet.com>
2023-07-06 13:09:27 +0100

    Initial commit [ammended with .gitattributes]

---
+++ Source/RiderTest/RiderTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "RiderTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RIDERTEST_API ARiderTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
